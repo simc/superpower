368147b7cb7e2dcdf21eacc530a2ba37b08602f1
Simon Leier <simonleier@gmail.com>
2018-09-14 00:26:01 +0200

    Add _$DelegatingIterable tests

--- test/delegating_iterable_test.dart
+++ test/delegating_iterable_test.dart
@@ -1,3 +1,107 @@
+import 'package:superpower/superpower.dart';
 import 'package:test/test.dart';
 
-void main() {}
+$Iterable<int> get empty => $Iterable<int>.empty();
+
+$Iterable<E> iterable<E>(List<E> elements) => $Iterable(elements).toIterable();
+
+void main() {
+  test('elementAt', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    expect(elements.elementAt(1), 1);
+    expect(elements.elementAt(4), 4);
+    expect(elements.elementAt(-1), 4);
+    expect(() => elements.elementAt(5), throwsRangeError);
+  });
+
+  test('iterator', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var i = 0;
+    for (var element in elements) {
+      expect(elements.elementAt(i++), element);
+    }
+  });
+
+  test('length', () {
+    expect(empty.length, 0);
+    expect(iterable([0, 1, 2, 3, 4]).length, 5);
+  });
+
+  test('followedBy', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    expect(empty.followedBy(empty), empty);
+    expect(empty.followedBy(elements), elements);
+    expect(elements.followedBy(elements), [0, 1, 2, 3, 4, 0, 1, 2, 3, 4]);
+    expect(elements.followedBy([1, 2]) is $Iterable, true);
+  });
+
+  test('map', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.map((e) => e * e);
+    expect(result, [0, 1, 4, 9, 16]);
+    expect(result is $Iterable, true);
+  });
+
+  test('where', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.where((e) => e > 2);
+    expect(result, [3, 4]);
+    expect(result is $Iterable, true);
+  });
+
+  test('whereType', () {
+    var elements = iterable([0, '1', 2, '3', 4]);
+    var result = elements.whereType<int>();
+    expect(result, [0, 2, 4]);
+    expect(result is $Iterable, true);
+  });
+
+  test('expand', () {
+    var elements = iterable([0, 1, 2]);
+    var result = elements.expand((e) => [e, e, e]);
+    expect(result, [0, 0, 0, 1, 1, 1, 2, 2, 2]);
+    expect(result is $Iterable, true);
+  });
+
+  test('toList', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    {
+      var result = empty.toList();
+      expect(result, <int>[]);
+      expect(result is $List, true);
+    }
+    {
+      var result = elements.toList();
+      expect(result, [0, 1, 2, 3, 4]);
+      expect(result is $List, true);
+    }
+  });
+
+  test('take', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.take(3);
+    expect(result, [0, 1, 2]);
+    expect(result is $Iterable, true);
+  });
+
+  test('takeWhile', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.takeWhile((e) => e < 3);
+    expect(result, [0, 1, 2]);
+    expect(result is $Iterable, true);
+  });
+
+  test('skip', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.skip(2);
+    expect(result, [2, 3, 4]);
+    expect(result is $Iterable, true);
+  });
+
+  test('skipWhile', () {
+    var elements = iterable([0, 1, 2, 3, 4]);
+    var result = elements.skipWhile((e) => e < 3);
+    expect(result, [3, 4]);
+    expect(result is $Iterable, true);
+  });
+}
